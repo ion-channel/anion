0e306a7d1ccf778c25f798e343c59b849391d5cc
Daniel Hess <dan9186@gmail.com>
2019-04-30 17:14:05 -0700

    exponse the new methods

--- anion.js
+++ anion.js
@@ -1,4 +1,5 @@
 export { Auth } from './lib/auth/auth'
-export { Teams } from './lib/teams/teams'
+export { Reports } from './lib/reports/reports'
 export { Sessions } from './lib/sessions/sessions'
+export { Teams } from './lib/teams/teams'
 export { Vulnerabilities } from './lib/vulnerabilities/vulnerabilities'
