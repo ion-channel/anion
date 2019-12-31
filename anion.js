d8d0bfd01c0e57ec1d89ad5a14ddf562f4f69987
Daniel Hess <dan9186@gmail.com>
2019-12-31 15:09:28 -0800

    expose deliveries functions

--- anion.js
+++ anion.js
@@ -1,4 +1,5 @@
 export { Auth } from './lib/auth/auth'
+export { Deliveries } from './lib/deliveries/deliveries'
 export { Reports } from './lib/reports/reports'
 export { Searches } from './lib/searches/searches'
 export { Sessions } from './lib/sessions/sessions'
